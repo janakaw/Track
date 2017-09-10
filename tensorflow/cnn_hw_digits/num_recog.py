58fc62eaf4dda5d9775557e8f6302d60f431028c
janaka seneviratne <janaka.seneviratne@gmail.com>
2017-09-10 21:11:11 +1000

    To recognise hand written digits

---
+++ tensorflow/cnn_hw_digits/num_recog.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import  matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+X = tf.placeholder(tf.float32, shape=[None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+init = tf.global_variables_initializer()
+
+#model
+Y = tf.nn.softmax(tf.matmul(X, W) + b)
+
+# placholder for correct answers
+Y_ = tf.placeholder(tf.float32, [None, 10])
+
+# loss function
+cross_entropy = -tf.reduce_sum(Y_ * tf.log(Y))
+
+# % of correct answer found in batch
+is_correct = tf.equal(tf.argmax(Y,1), tf.argmax(Y_,1))
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+optimizer = tf.train.GradientDescentOptimizer(0.003)
+train_step = optimizer.minimize(cross_entropy)
+
+sess = tf.Session()
+sess.run(init)
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+acc = []
+ce = []
+x = []
+
+for i in range(100):
+	#print str(i)
+	# load batch of images and correct answers
+	batch_X, batch_Y = mnist.train.next_batch(100)
+	#print str(len(batch_X)) + " " + str(len(batch_X[0])) + " " + str(len(batch_Y)) + " " + str(len(batch_Y[0]))
+	#print size(batch_Y)
+	train_data={X: batch_X, Y_:batch_Y}
+
+	# train 
+	sess.run(train_step, train_data)
+
+	a, c = sess.run([accuracy, cross_entropy], train_data)
+	x.append(i)
+	acc.append(a)
+	ce.append(c)
+
+	test_data = {X:mnist.test.images, Y_:mnist.test.labels}
+	a,c = sess.run([accuracy, cross_entropy], test_data)
+
+plt.plot(acc)
+plt.ylabel('accuracy')
+plt.show()
+
+plt.plot(ce)
+plt.ylabel('cross entropy')
+plt.show()
+
+
+
